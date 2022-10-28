380bd090b488560fa8c66225e4f0595ffa2f9ab6
Silvan Schlegel <silvanschlegel@student-net-rz-1039.intern.ethz.ch>
2022-10-28 18:34:51 +0200

    Hashtag Analysis fertig

--- Übung 6/Hashtag Analysis/public/script.py
+++ Übung 6/Hashtag Analysis/public/script.py
@@ -4,19 +4,26 @@ import re
 # Do not rename the function or change its list of parameters.
 def analyze(posts):
     hashtags = {}
+    words = []
+    tags = []
     for i in posts:
-        words = re.split(r'[`!@$%^&*()_+\-=\[\]{};\':"\\|,.<>\/?~ ]', i)
-        for j in words:
-            while j.startswith("#"):
-                if j.startswith("#"):
-                    j = j[1:]
-                    if not len(j) == 0:
-                        if not j[0].isdigit():
-                            if j.isalnum():
-                                if j in hashtags.keys():
-                                    hashtags[j] += 1
-                                else:
-                                    hashtags[j] = 1
+        words += re.split(r'[`!@$%^&*()_+\-=\[\]{};\':"\\|,.<>\/?~ ]', i)
+    for l in words:
+        if "#" in l:
+            tags += l.split("#")
+    for j in tags:
+        while j.startswith("#"):
+            if j.startswith("#"):
+                j = j[1:]
+        if not len(j) == 0:
+            if not j[0].isdigit():
+                if j.isalnum():
+                    if j in hashtags.keys():
+                        hashtags[j] += 1
+                    else:
+                        hashtags[j] = 1
+
+
 
     return hashtags
 
@@ -27,7 +34,7 @@ def analyze(posts):
 # can easily test many different values on every "Test & Run"!
 posts = [
     "hi #weekend",
-    "good morning #zurich #limmat .#c. ...:#dasistok.dasnichtmehr",
-    "spend my #weekend in #zurich",
-    "#zurich <3 #1hallo #hallo ##aa #a#a"]
+    "good morning #zurich #limmat .#c. #1hallo...:#dasistok.dasnichtmehr",
+    "spend my #weekend in #zurich #",
+    "#zurich <3 #1hallo #hallo ##aa #a#a #tabea#.#test"]
 print(analyze(posts))
