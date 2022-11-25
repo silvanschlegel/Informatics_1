11326dd56b2175f9f0d0842d04fbdbf4bf13d9c2
Silvan Schlegel <silvanschlegel@student-net-rz-1039.intern.ethz.ch>
2022-11-25 11:48:25 +0100

    Profanity Filter fertig

--- Übung 8/Profanity Filter/public/script.py
+++ Übung 8/Profanity Filter/public/script.py
@@ -6,16 +6,40 @@
 class ProfanityFilter:
 
     def __init__(self, keywords, template):
-        pass
+        self.__keywords = keywords
+        self.__template = template
 
     def filter(self, msg):
-        pass
+        if not isinstance(msg, str):
+            raise Warning("Input must be string")
+
+        if msg == "":
+            return ""
+
+        word_list = msg.split()
+        for k, i in enumerate(word_list):
+            for j in reversed(sorted(self.__keywords, key=len)):
+                if j.lower() in i.lower():
+                    word_list[k] = self.__clean(i.lower())
+        return ' '.join(word_list)
+
+    def __clean(self, word):
+        cleaned = ""
+        j = 0
+        for i in reversed(sorted(self.__keywords, key=len)):
+            if i.lower() in word:
+                for j in range(len(i)):
+                    cleaned += self.__template[j % (len(self.__template))]
+                    j += 1
+                word = word.replace(i.lower(), cleaned)
+                cleaned = ""
+        return word
 
 
 # You can play around with your implementation in the body of the following 'if'.
 # The contained statements will be ignored while evaluating your solution.
 if __name__ == '__main__':
     f = ProfanityFilter(["duck", "shot", "batch", "mastard"], "?#$")
-    offensive_msg = "abc defghi mastard jklmno"
+    offensive_msg = "abc defghi Mastardsilvanduck jklmno"
     clean_msg = f.filter(offensive_msg)
     print(clean_msg)  # abc defghi ?#$?#$? jklmno
--- Übung 8/Profanity Filter/public/tests.py
+++ Übung 8/Profanity Filter/public/tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from unittest import TestCase
-from public.script import ProfanityFilter
+from script import ProfanityFilter
 
 
 class PublicTestSuite(TestCase):
@@ -12,6 +12,48 @@ class PublicTestSuite(TestCase):
         actual = f.filter(msg)
         expected = "abc defghi ?#$?#$? jklmno"
         self.assertEqual(expected, actual)
+
+    def test_emptyinput(self):
+        f = ProfanityFilter(["duck", "shot", "batch", "mastard"], "?#$")
+        msg = ""
+        actual = f.filter(msg)
+        expected = ""
+        self.assertEqual(expected, actual)
+
+    def test_input_false(self):
+        f = ProfanityFilter(["duck", "shot", "batch", "mastard"], "?#$")
+        self.assertRaises(Warning, f.filter, 10)
+
+    def test_keywords_sorted(self):
+        f = ProfanityFilter(["duck", "shot", "batch", "mastard"], "?#$")
+        msg = "mastardduck"
+        actual = f.filter(msg)
+        expected = "?#$?#$??#$?"
+        self.assertEqual(expected, actual)
+
+    def test_two_words_in_1_string(self):
+        f = ProfanityFilter(["duck", "shot", "batch", "mastard"], "?#$")
+        msg = "mastardduck"
+        actual = f.filter(msg)
+        expected = "?#$?#$??#$?"
+        self.assertEqual(expected, actual)
+
+
+    def test_case_insensitive(self):
+        f = ProfanityFilter(["duck", "shot", "batch", "mastard"], "?#$")
+        msg = "abc defghi MAstard jklmno"
+        actual = f.filter(msg)
+        expected = "abc defghi ?#$?#$? jklmno"
+        self.assertEqual(expected, actual)
+
+    def test_other_template(self):
+        f = ProfanityFilter(["instagram", "staudenstrasse9a", "9472Grabs", "mastard"], "silvan")
+        msg = "Hallo, mein Name ist Silvan und ich wohne an der Staudenstrasse9a in 9472grabs mastard instagram"
+        actual = f.filter(msg)
+        expected = "Hallo, mein Name ist Silvan und ich wohne an der silvansilvansilv in silvansil silvans silvansil"
+        self.assertEqual(expected, actual)
+
+
 
     # This current test suite only contains one very basic test case. By now,
     # you have some experience in writing test cases. We strongly ecncourage
