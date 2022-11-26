8e7c8e64d430580a5ffd5b9bf4ff5fa25a582b87
Silvan Schlegel <silvanschlegel@student-net-rz-1039.intern.ethz.ch>
2022-11-26 07:41:30 +0100

    Publication done

--- Übung 8/Publication/public/script.py
+++ Übung 8/Publication/public/script.py
@@ -6,10 +6,149 @@
 class Publication:
 
     def __init__(self, authors, title, year):
-        pass
+        self.__authors = authors
+        self.__title = title
+        self.__year = year
 
     # To implement the required functionality, you will also have to implement several
     # of the special functions that typically include a double underscore.
+    # implement get_authors
+    def get_authors(self):
+        temp = self.__authors[:]
+        return temp
+
+    # implement get_title
+    def get_title(self):
+        return self.__title
+
+    # implement get_year
+    def get_year(self):
+        return self.__year
+
+    # implement __repr__
+    def __repr__(self):
+        from json import dumps
+        return f"Publication({dumps(self.__authors)}, \"{self.__title}\", {self.__year})"
+
+    # implement __str__
+    def __str__(self):
+        from json import dumps
+        return f"Publication({dumps(self.__authors)}, \"{self.__title}\", {self.__year})"
+
+    # implement __eq__
+    def __eq__(self, other):
+        if isinstance(other, Publication):
+            if self.__authors == other.__authors:
+                if self.__title == other.__title:
+                    if self.__year == other.__year:
+                        return True
+                    return False
+                return False
+            return False
+        return NotImplemented
+
+    # implement __hash__
+    def __hash__(self):
+        return hash((tuple(self.__authors), self.__title, self.__year))
+
+    # implement __lt__
+    def __lt__(self, other):
+        if not isinstance(other, Publication):
+            return NotImplemented
+        # check names
+        if len(self.__authors) < len(other.__authors):
+            return True
+        if len(self.__authors) > len(other.__authors):
+            return False
+        #gleich viele namen
+        for i, j in enumerate(self.__authors):
+            if self.__authors[i] < other.__authors[i]:
+                return True
+
+        # check title
+        if self.__title < other.__title:
+            return True
+
+        # check year
+        if self.__year < other.__year:
+            return True
+
+        return False
+
+    # implement __le__
+    def __le__(self, other):
+        if not isinstance(other, Publication):
+            return NotImplemented
+        # check names
+        if not len(self.__authors) <= len(other.__authors):
+            return False
+
+        for i, j in enumerate(self.__authors):
+            if not self.__authors[i] <= other.__authors[i]:
+                return False
+        # check title
+        if not self.__title <= other.__title:
+            return False
+
+        # check year
+        if not self.__year <= other.__year:
+            return False
+
+        return True
+
+    # implement __gt__
+    def __gt__(self, other):
+        if not isinstance(other, Publication):
+            return NotImplemented
+        # check names
+        if len(self.__authors) > len(other.__authors):
+            return True
+        if len(self.__authors) < len(other.__authors):
+            return False
+        for i, j in enumerate(other.__authors):
+            if self.__authors[i] > other.__authors[i]:
+                return True
+
+        # check title
+        if self.__title > other.__title:
+            return True
+
+        # check year
+        if self.__year > other.__year:
+            return True
+
+        return False
+
+    # implement __ge__
+    def __ge__(self, other):
+        if not isinstance(other, Publication):
+            return NotImplemented
+        # check names
+        if not len(self.__authors) >= len(other.__authors):
+            return False
+        for i, j in enumerate(other.__authors):
+            if not self.__authors[i] >= other.__authors[i]:
+                return False
+        # check title
+        if not self.__title >= other.__title:
+            return False
+
+        # check year
+        if not self.__year >= other.__year:
+            return False
+
+        return True
+
+    def __ne__(self, other):
+        if not isinstance(other, Publication):
+            return NotImplemented
+        if self.__authors == other.__authors:
+            if self.__title == other.__title:
+                if self.__year == other.__year:
+                    return False
+                return True
+            return True
+        return True
 
 
 # You can play around with your implementation in the body of the following 'if'.
@@ -26,13 +165,27 @@ if __name__ == '__main__':
     print(p)
     print(str(p) == s)
 
-    p1 = Publication(["A"], "B", 1234)
-    p2 = Publication(["A"], "B", 1234)
+    p1 = Publication(["Schlegel", "Waltenspuel"], "Wurmhoehle 17", 2022)
+    p2 = Publication(["Schlegel"], "Wurmhoehle 17", 2022)
     p3 = Publication(["B"], "C", 2345)
-    print(p1 == p2)  # True
-    print(p2 == p3)  # False
+    print(p1 >= p2)  # True
+    #print(p2 == p3)  # False
+    #print(p1 < p2)
+    print(p.get_authors())
 
     sales = {
         p1: 273,
-        p2: 398,
+        p3: 398,
     }
+
+    for values in sales.values():
+        print(values)
+    for key in sales.keys():
+        print(key)
+
+    a = [1,2,3,4]
+    b = a[:]
+    b[1] = "mutable"
+    print(a)
+    print(b)
+    print("aaaaa">"aaaaa")
--- Übung 8/Publication/public/tests.py
+++ Übung 8/Publication/public/tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from unittest import TestCase
-from public.script import Publication
+from script import Publication
 
 
 class PublicTestSuite(TestCase):
@@ -10,6 +10,149 @@ class PublicTestSuite(TestCase):
         a = Publication(["A"], "B", 1234)
         b = Publication(["A"], "B", 1234)
         self.assertEqual(a, b)
+
+    def test_repr_(self):
+        a = Publication(["A"], "B", 1234)
+        b = "Publication([\"A\"], \"B\", 1234)"
+        self.assertEqual(str(a), b)
+
+    def test_str_(self):
+        a = Publication(["A"], "B", 1234)
+        b = "Publication([\"A\"], \"B\", 1234)"
+        self.assertEqual(str(a), b)
+
+    def test_gt_1(self):
+        p1 = Publication(["Schlegel", "Waltenspül"], "Wurmhöhle 17", 2022)
+        p2 = Publication(["Schlegel"], "Wurmhöhle 17", 2022)
+        self.assertEqual(p1 > p2, True)
+
+    def test_gt_2(self):
+        p1 = Publication(["Schlegel", "Waltenspül"], "Wurmhöhle 17", 2022)
+        p2 = Publication(["Schlegel", "A", "B"], "Wurmhöhle 17", 2022)
+        self.assertEqual(p1 > p2, False)
+
+    def test_gt_3(self):
+        p1 = Publication(["Schlegel"], "Wurmhöhle 17", 2021)
+        p2 = Publication(["Schlegel"], "Wurmhöhle 17", 2022)
+        self.assertEqual(p1 > p2, False)
+
+    def test_gt_3(self):
+        p1 = Publication(["Schlegel"], "b", 2021)
+        p2 = Publication(["Schlegel"], "a", 2022)
+        self.assertEqual(p1 > p2, True)
+
+    def test_lt_1(self):
+        p1 = Publication(["Schlegel", "Waltenspül"], "Wurmhöhle 17", 2022)
+        p2 = Publication(["Schlegel"], "Wurmhöhle 17", 2022)
+        self.assertEqual(p2 < p1, True)
+
+    def test_lt_2(self):
+        p1 = Publication(["Schlegel", "Waltenspül"], "Wurmhöhle 17", 2022)
+        p2 = Publication(["Schlegel"], "Wurmhöhle 17", 2022)
+        self.assertEqual(p1 < p2, False)
+
+    def test_lt_3(self):
+        p1 = Publication(["Schlegel"], "ab", 2022)
+        p2 = Publication(["Schlegel"], "aa", 2022)
+        self.assertEqual(p1 < p2, False)
+
+    def test_lt_4(self):
+        p1 = Publication(["Schlegel"], "aaa", 2022)
+        p2 = Publication(["Schlegel"], "aa", 2022)
+        self.assertEqual(p1 < p2, False)
+
+    def test_lt_5(self):
+        p1 = Publication(["Schlegel"], "a", 2022)
+        p2 = Publication(["Schlegel"], "aa", 2022)
+        self.assertEqual(p1 < p2, True)
+
+    def test_ge_1(self):
+        p1 = Publication(["Schlegel", "Waltenspuel"], "Wurmhoehle 17", 2022)
+        p2 = Publication(["Schlegel"], "Wurmhoehle 17", 2022)
+        self.assertEqual(p1 >= p2, True)
+
+    def test_ge_2(self):
+        p1 = Publication(["Schlegel", "Waltenspuel"], "Wurmhoehle 17", 2022)
+        p2 = Publication(["Schlegel", "Waltenspuel", "Zuerich"], "Wurmhoehle 17", 2022)
+        self.assertEqual(p1 >= p2, False)
+
+    def test_ge_3(self):
+        p1 = Publication(["Schlegel", "Waltenspuel"], "aa", 2022)
+        p2 = Publication(["Schlegel", "Waltenspuel"], "a", 2022)
+        self.assertEqual(p1 >= p2, True)
+
+    def test_ge_4(self):
+        p1 = Publication(["Schlegel", "Waltenspuel"], "aa", 2022)
+        p2 = Publication(["Schlegel", "Waltenspuel"], "aaa", 2022)
+        self.assertEqual(p1 >= p2, False)
+
+    def test_ge_5(self):
+        p1 = Publication(["Schlegel", "Waltenspuel"], "a", 2020)
+        p2 = Publication(["Schlegel", "Waltenspuel"], "a", 2022)
+        self.assertEqual(p1 >= p2, False)
+
+    def test_ge_6(self):
+        p1 = Publication(["Schlegel", "Waltenspuel"], "aa", 2022)
+        p2 = Publication(["Schlegel", "Waltenspuel"], "aa", 2022)
+        self.assertEqual(p1 >= p2, True)
+
+
+    def test_le_1(self):
+        p1 = Publication(["Schlegel", "Waltenspül"], "Wurmhöhle 17", 2022)
+        p2 = Publication(["Schlegel"], "Wurmhöhle 17", 2022)
+        self.assertEqual(p2 <= p1, True)
+
+    def test_le_2(self):
+        p1 = Publication(["Schlegel", "Waltenspül"], "Wurmhöhle 17", 2022)
+        p2 = Publication(["Schlegel"], "Wurmhöhle 17", 2022)
+        self.assertEqual(p1 <= p2, False)
+
+    def test_le_3(self):
+        p1 = Publication(["Schlegel"], "aaa", 2022)
+        p2 = Publication(["Schlegel"], "aa", 2022)
+        self.assertEqual(p1 <= p2, False)
+
+    def test_le_4(self):
+        p1 = Publication(["Schlegel"], "aa", 2022)
+        p2 = Publication(["Schlegel"], "aa", 2022)
+        self.assertEqual(p1 <= p2, True)
+
+    def test_le_5(self):
+        p1 = Publication(["Schlegel"], "a", 2022)
+        p2 = Publication(["Schlegel"], "aa", 2022)
+        self.assertEqual(p1 <= p2, True)
+
+    def test_ne1(self):
+        p1 = Publication(["Schlegel", "Waltenspül"], "Wurmhöhle 17", 2022)
+        p2 = Publication(["Schlegel"], "Wurmhöhle 17", 2022)
+        self.assertEqual(p1 != p2, True)
+
+    def test_ne2(self):
+        p1 = Publication(["Schlegel"], "Wurmhöhle 17", 2022)
+        p2 = Publication(["Schlegel"], "Wurmhöhle 17", 2022)
+        self.assertEqual(p1 != p2, False)
+
+    def test_eq_1(self):
+        p1 = Publication(["Schlegel", "Waltenspül"], "Wurmhöhle 17", 2022)
+        p2 = Publication(["Schlegel"], "Wurmhöhle 17", 2022)
+        self.assertEqual(p1 == p2, False)
+
+    def test_eq_2(self):
+        p1 = Publication(["Schlegel"], "Wurmhöhle 17", 2022)
+        p2 = Publication(["Schlegel"], "Wurmhöhle 17", 2022)
+        self.assertEqual(p1 == p2, True)
+
+    def test_eq_3(self):
+        p1 = Publication(["Schlegel"], "Wurmhöh", 2022)
+        p2 = Publication(["Schlegel"], "Wurmhöhle 17", 2022)
+        self.assertEqual(p1 == p2, False)
+
+
+
+
+
+
+
 
 
 
